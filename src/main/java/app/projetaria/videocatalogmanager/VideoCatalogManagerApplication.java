63a7f77fbd565a1fb6fb68d3f15d2194cecd5a31
Danilo Tiago <danilotiago@Danilos-MacBook-Pro.local>
2022-04-18 23:18:40 -0300

    initial commit

---
+++ src/main/java/app/projetaria/videocatalogmanager/VideoCatalogManagerApplication.java
@@ -0,0 +1,13 @@
+package app.projetaria.videocatalogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCatalogManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCatalogManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/projetaria/videocatalogmanager/VideoCatalogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.projetaria.videocatalogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCatalogManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
